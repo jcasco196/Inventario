f406b8b249dae401cd0160cb4ed15ed24f246269
Jesus <jcasco196@gmail.com>
2020-02-04 16:17:52 +0100

    Prueba subida

--- app/src/main/java/com/example/inventario/Registro.java
+++ app/src/main/java/com/example/inventario/Registro.java
@@ -3,6 +3,8 @@ package com.example.inventario;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import android.app.ProgressDialog;
@@ -10,6 +12,7 @@ import android.content.Intent;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,6 +24,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Calendar;
+
 public class Registro extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etDisplayName;
@@ -30,8 +35,10 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
     private Button btnRegistrarse;
     private FirebaseAuth.AuthStateListener firebaseAuthListener;
     private TextView tvEntrar;
-
+    private Calendar calendar;
+    private int year, month, day;
     private ProgressDialog progressDialog;
+    private TextView cumple;
 
     private DatabaseReference mDatabase;
     private FirebaseAuth firebaseAuth;
@@ -64,14 +71,45 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
         etPassword2 = (EditText) findViewById(R.id.password2);
         etPassword = (EditText) findViewById(R.id.password);
         tvEntrar = (TextView) findViewById(R.id.entrar);
+        cumple = (TextView) findViewById(R.id.cumple);
         btnRegistrarse = (Button) findViewById(R.id.registrarse);
 
         progressDialog = new ProgressDialog(this);
 
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
         btnRegistrarse.setOnClickListener(this);
         tvEntrar.setOnClickListener(this);
     }
 
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "Veamos tu fecha", Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
+
+    private void showDate(int year, int month, int day) {
+        cumple.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
     @Override
     protected void onStart() {
         super.onStart();
@@ -83,6 +121,7 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
 
         final String displayName = etDisplayName.getText().toString().trim();
         String email = etEmail.getText().toString().trim();
+        String aniversario = cumple.getText().toString().trim();
         String password2 = etPassword2.getText().toString().trim();
         String password  = etPassword.getText().toString().trim();
 
@@ -93,6 +132,11 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
 
         if(TextUtils.isEmpty(email)){
             Toast.makeText(this,"Porfavor ponga un email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(aniversario)){
+            Toast.makeText(this,"Porfavor ponga una fecha de nacimiento", Toast.LENGTH_LONG).show();
             return;
         }
 
@@ -137,7 +181,8 @@ public class Registro extends AppCompatActivity implements View.OnClickListener 
     private void onAuthSuccess(FirebaseUser user) {
         String email = etEmail.getText().toString();
         String nombre = etDisplayName.getText().toString();
-        mDatabase.child("users").child(user.getUid()).setValue(new User(user.getUid(), nombre, email, null));
+        String nacimiento = cumple.getText().toString();
+        mDatabase.child("users").child(user.getUid()).setValue(new User(user.getUid(), nombre, email, nacimiento));
 
         startActivity(new Intent(Registro.this, Perfil.class));
         finish();
